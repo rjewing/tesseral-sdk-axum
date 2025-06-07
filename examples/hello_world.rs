4ac37c339cc126ce82e0b97fef71cf04c02e889c
Ulysse Carion <ulysse.carion@ssoready.com>
2025-06-07 15:17:09 -0700

    about to reset

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,35 +1,16 @@
-// //! Hello World example for tesseral-axum
-// //!
-// //! This example demonstrates a simple Axum web server that responds with "Hello, World!"
-// //! to HTTP requests.
-//
-use axum::{middleware, routing::get, Extension, Router};
+use axum::{routing::get, Router};
 use std::net::SocketAddr;
-use tesseral_axum::auth::Auth;
+use tesseral_axum::{require_auth, Authenticator};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    // Initialize tracing for better debugging
-    tracing_subscriber::fmt::init();
-
-    let require_auth_layer = tesseral_axum::auth_layer::RequireAuthBuilder::new(
-        tesseral_axum::access_token_authenticator::AuthenticatorBuilder::new(
-            "publishable_key_en43cawcravxk7t2murwiz192".to_owned(),
-        )
-        .build(),
-    )
-    .with_backend_api_client(
-        tesseral_axum::backend_api::ClientBuilder::new()
-            .backend_api_key("asdf")
-            .build(),
-    )
-    .build();
+    let authenticator = Authenticator::new();
 
     // Build our application with a single route
     let app = Router::new()
         .route("/", get(handler))
-        .layer(require_auth_layer);
+        .layer(require_auth(authenticator));
 
     // Run the server
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -39,11 +20,6 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-// Our handler function
-async fn handler(Extension(auth): Extension<Auth>) -> String {
-    dbg!(auth.organization_id());
-    dbg!(auth.credentials());
-    dbg!(auth.access_token_claims());
-    dbg!(auth.has_permission("foo.bar.baz"));
+async fn handler() -> String {
     "Hello, World!".to_owned()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,122 +1,14 @@
-pub mod backend_api;
-pub mod access_token_authenticator;
-mod access_token_claims;
-pub mod auth_layer;
-mod credentials;
-pub mod auth;
+use std::future::Future;
+use axum::extract::Request;
+use axum::middleware::{FromFnLayer, Next};
+use axum::response::Response;
+use tower::{Layer, Service};
 
-pub use credentials::{is_jwt_format, is_api_key_format};
 
-use serde::{Deserialize, Serialize};
-//
-// use tower::Layer;
-// use axum::extract::Request;
-// use axum::http::StatusCode;
-// use axum::middleware::Next;
-// use axum::response::Response;
-//
-// #[derive(Clone)]
-// pub struct Auth {
-//     auth_data: AuthData,
-// }
-//
-// #[derive(Clone)]
-// enum AuthData {
-//     AccessToken(AccessTokenData),
-//     ApiKey(ApiKeyData),
-// }
-//
-// impl Auth {
-//     pub fn credentials(&self) -> &str {
-//         match &self.auth_data {
-//             AuthData::AccessToken(token_data) => &token_data.credentials,
-//             AuthData::ApiKey(key_data) => &key_data.api_key_secret_token,
-//         }
-//     }
-//
-//     pub fn access_token_claims(&self) -> Option<AccessTokenClaims> {
-//         match &self.auth_data {
-//             AuthData::AccessToken(token_data) => Some(token_data.claims.clone()),
-//             AuthData::ApiKey(_) => None,
-//         }
-//     }
-//
-//     pub fn has_permission(&self, action: &str) -> bool {
-//         match &self.auth_data {
-//             AuthData::AccessToken(token_data) => token_data
-//                 .claims
-//                 .actions
-//                 .as_ref()
-//                 .map_or(false, |actions| actions.contains(&action.to_string())),
-//             AuthData::ApiKey(key_data) => key_data
-//                 .authenticate_api_key_response
-//                 .actions
-//                 .contains(&action.to_string()),
-//         }
-//     }
-//
-//     pub fn credentials_type(&self) -> CredentialsType {
-//         match &self.auth_data {
-//             AuthData::AccessToken(_) => CredentialsType::AccessToken,
-//             AuthData::ApiKey(_) => CredentialsType::ApiKey,
-//         }
-//     }
-// }
-//
-// #[derive(Clone, Debug)]
-// pub enum CredentialsType {
-//     AccessToken,
-//     ApiKey,
-// }
-//
-// #[derive(Clone, Debug, Serialize, Deserialize)]
-// struct AccessTokenData {
-//     credentials: String,
-//     claims: AccessTokenClaims,
-// }
-//
-//
-// #[derive(Clone, Debug)]
-// struct ApiKeyData {
-//     api_key_secret_token: String,
-//     authenticate_api_key_response: AuthenticateApiKeyResponse,
-// }
-//
-// #[derive(Clone, Debug, Serialize, Deserialize)]
-// struct AuthenticateApiKeyResponse {
-//     #[serde(rename = "apiKeyId")]
-//     api_key_id: String,
-//     #[serde(rename = "organizationId")]
-//     organization_id: String,
-//     #[serde(rename = "actions")]
-//     actions: Vec<String>,
-// }
-//
-// pub struct Options {
-//     pub publishable_key: String,
-//     pub config_api_hostname: String,
-//     pub jwks_refresh_interval_seconds: i64,
-//     pub api_keys_enabled: bool,
-//     pub backend_api_client: reqwest::Client,
-// }
-//
-// pub struct RequireAuthLayer {
-//
-// }
-//
-// impl Layer<S> for RequireAuthLayer {
-//     type Service = RequireAuthService<S>;
-//
-//     fn layer(&self, inner: S) -> Self::Service {
-//         todo!()
-//     }
-// }
-//
-// pub struct RequireAuthService<S> {
-//     inner: S,
-//
-// }
-//
-// pub async fn require_auth(mut req: Request, next: Next) -> Result<Response, StatusCode> {
-//     Ok(next.run(req).await)
-// }
+pub struct Authenticator {}
+
+impl Authenticator {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
